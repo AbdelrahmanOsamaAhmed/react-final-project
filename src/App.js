c2f0bae14009eff7617f1f53ffaf03d1ce271848
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2022-09-24 17:24:13 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import LogIn from "./components/login/login";
 
 function App() {
   return (
     <>
-      
+      <LogIn />
     </>
   );
 }
---
+++ src/components/login/login.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./login.css";
+
+
+export default function LogIn() {
+  return (
+    <>
+      
+    </>
+  );
+}
