926a884baf1d4206ac5ef23c964098472fd6f8fb
NIGM <ahmednigm111@gmail.com>
2022-09-26 10:37:32 +0200

    Update Connections.js

--- src/Components/Connections.js
+++ src/Components/Connections.js
@@ -25,7 +25,15 @@ export default function Connections(){
             {Connectionslist.map((conn)=>{
                 return(
                     <div class="card text-bg-dark col-xxl-2 col-xl-2 col-lg-2 col-md-3 col-sm-4 col-8 m-2 ">
-                    {<button id={`${conn.id}`} className="position-absolute bg-transparent  border-0 end-0 m-2" style={{width:'fit-content'}} onClick={()=>{connect(conn) ; }}>
+                    {<button id={`${conn.id}`} className="position-absolute bg-transparent  border-0 end-0 m-2" style={{width:'fit-content'}} onClick={()=>{
+                        connect(conn) ; 
+                        let newconn=Connectionslist.filter((i)=>{
+                            
+                            if(i.id==conn.id)console.log(i)
+                            else return i
+                        })
+                        setConnectionslist(newconn)
+                         }}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="green" class="bi bi-plus-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
