b3a438fa661352a9446109dd540d4baf7d096e00
Ziad Ashraf <zashraf011@gmail.com>
2022-09-27 21:29:02 +0200

    sidebar-responsive

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import SidePanel from "./components/side panel/sidePanel";
 import Reels from "./components/Reels/Reels";
 import Connections from "./components/connection/Connections";
-import store from "./store/store";
-import { Provider } from 'react-redux';
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LogIn from "./components/login/login";
@@ -15,21 +13,21 @@ import About from "./components/about/about";
 function App() {
   return (
     <>
-    <Router>
-      <SidePanel/>
-      <Route path="/login" exact component={LogIn} />
-      <Route path="/signup" exact component={SignUp} />
-      <Route path="/connections" exact component={Connections} />
-      <Route path="/reels" exact component={Reels} />
-      <Route path="/profile" exact component={ViewProfile} />
-      <Route path="/about" exact component={About} />
-    </Router>
-  </>
-  )
+      <Router>
+        <div className="d-flex">
+          <SidePanel />
+          <Route path="/login" exact component={LogIn} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/connections" exact component={Connections} />
+          <Route path="/reels" exact component={Reels} />
+          <Route path="/profile" exact component={ViewProfile} />
+          <Route path="/about" exact component={About} />
+        </div>
+      </Router>
+    </>
+  );
 
-
-
-/* function App() {
+  /* function App() {
   return (
     
 
