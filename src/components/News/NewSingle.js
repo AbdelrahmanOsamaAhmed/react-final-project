448e7c18a0efb9f07b0c633cfe4d267b23982876
Saam22 <88535528+Saam22@users.noreply.github.com>
2022-09-27 16:01:32 -0700

    Add files via upload

---
+++ src/components/News/NewSingle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./style.css";
+const NewSingle = ({ item }) => (
+
+    <div className="card">
+        <div className="card-image">
+            <img className="card-img-top" src={item.urlToImage} alt={item.title} />
+
+        </div>
+        <h5>{item.title}</h5>
+        <div className="card-action">
+            <a href={item.url} className="btn btn-primary" target="_blank">Read More</a>
+        </div>
+    </div>
+);
+
+export default NewSingle;
